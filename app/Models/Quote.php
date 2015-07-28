9ed0e96c614b37af3a8a4c584c7e2d549d25bf0b
Olatunde Alex-Oni <tunde.alexoni@yahoo.com>
2015-07-28 15:39:52 +0100

    initial push

---
+++ app/Models/Quote.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Models;
+
+/**
+ * Description of Quote
+ *
+ * @author Irokotv
+ */
+use Illuminate\Database\Eloquent\Model;
+
+final class Quote extends Model {
+    //put your code here
+    
+}
