e486097ccb4af8cfeb7ec87dbf912b0ad8256b6b
Olatunde Alex-Oni <tunde.alexoni@yahoo.com>
2015-07-29 16:21:00 +0100

    debug

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Voucher;
+use \App\Lib\Voucher\Data;
 
 /**
  * Description of VoucherController
@@ -13,8 +13,25 @@ use App\Voucher;
 
 class VoucherController extends Controller {
     
+    const OK = 200;
+    const UNAUTHORIZED = 401;
+    CONST ERROR = 500;
+    
     public function getVoucher(){
-        echo 'Lights , Camera ,Action';
+        $dataObject = new Data();
+        $data = array();
+        $data["response-code"] = self::OK ;
+        $data["message"] = "OK" ;
+        $start = time();
+        
+        if($dataObject->getCode()){
+            $data["code"] =  $dataObject->getCode();
+            
+         }else{
+            $data["code"] =  0;
+        }
+
+        return response()->json($data);
     }
     
 }
