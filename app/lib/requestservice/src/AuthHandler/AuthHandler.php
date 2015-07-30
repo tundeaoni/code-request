59f69ba89701e8ec9ee9b820901eb9182cdfdf42
Sanni Abdulmusawwir <abdulmusawwir@irokotv.com>
2015-07-30 09:14:49 +0100

    Added authentication codes to AuthHandler

--- app/lib/requestservice/src/AuthHandler/AuthHandler.php
+++ app/lib/requestservice/src/AuthHandler/AuthHandler.php
@@ -3,6 +3,9 @@
 namespace RequestService\AuthHandler;
 
 abstract class AuthHandler {
+    const AUTH_SUCCESS = 0;
+    const AUTH_FAILED  = 1;
+
     abstract public function authenticate();
 
     abstract public function authorize();
