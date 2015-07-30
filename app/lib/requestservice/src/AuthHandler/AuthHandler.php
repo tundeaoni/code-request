b1a891028ab69251bcca36e3562156cded803dec
Sanni Abdulmusawwir <abdulmusawwir@irokotv.com>
2015-07-30 10:18:13 +0100

    Added auth messages to auth handler class

--- app/lib/requestservice/src/AuthHandler/AuthHandler.php
+++ app/lib/requestservice/src/AuthHandler/AuthHandler.php
@@ -6,6 +6,9 @@ abstract class AuthHandler {
     const AUTH_SUCCESS = 0;
     const AUTH_FAILED  = 1;
 
+    const AUTH_MSG_SUCCESS  = 'successful';
+    const AUTH_MSG_FAILED   = 'failed';
+
     abstract public function authenticate();
 
     abstract public function authorize();
--- app/lib/requestservice/src/RequestLogger.php
+++ app/lib/requestservice/src/RequestLogger.php
@@ -5,7 +5,11 @@ class RequestLogger {
     public function log($email) {
         // get token storage
         $token_dir = storage_path() . '/tokens/';
+
+        // create new verification token filename (from email)
         $filename = $token_dir . md5($email);
+
+        // generate token, store in token file and return token
         $token = $this->generateToken();
         file_put_contents($filename, $token);
         return $token;
